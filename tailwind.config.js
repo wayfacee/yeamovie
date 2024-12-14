293f6705d10c1b862f2c44a73aeb185419e1d99c
Wayface <wayfacee@gmail.com>
2024-12-15 01:40:07 +0600

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -54,5 +56,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate], // [require("tailwindcss-animate")],
 };
