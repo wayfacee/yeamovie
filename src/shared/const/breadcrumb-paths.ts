f1c38084f93ff12d4be87182d5ebad47b7ab2ebe
Wayface <wayfacee@gmail.com>
2024-12-17 22:43:27 +0600

    че то еще

--- src/shared/const/breadcrumb-paths.ts
+++
@@ -1,6 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-const navigate = useNavigate();
-const paths = [{ name: "Назад", onClick: () => navigate(-1) }];
-
-export const BREADCRUMB_PATHS = [];
